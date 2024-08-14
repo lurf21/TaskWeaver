5e44cb9baf8acf2d6aaf926674570e19b9319482
lurf21 <2953096035@qq.com>
2024-08-14 11:45:43 +0800

    sudo group

--- docker/all_in_one_container/entrypoint.sh
+++ docker/all_in_one_container/entrypoint.sh
@@ -6,6 +6,7 @@ GROUP_ID=${TASKWEAVER_GID:-10002}
 echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
 useradd -u $USER_ID -o -m taskweaver
 groupmod -g $GROUP_ID taskweaver
+usermod -aG sudo taskweaver
 
 chown -R taskweaver:taskweaver /app
 
